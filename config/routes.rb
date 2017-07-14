5435b3534c3165fae1dd7c74b5ac765e9e9daf53
Mxim0s <sushildamdhere@rediffmail.com>
2017-07-14 23:40:20 +0530

    Added secretes and root path

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/', to: redirect('/api-docs')
 
   namespace :api do
     namespace :v1 do
